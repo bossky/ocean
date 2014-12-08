8652d4403d32b8b805588dcbfa7d3b6107d00280
HelloWorld <bossky@users.noreply.github.com>
2014-12-08 22:00:25 +0800

    优化代码

--- src/com/bossky/ocean/user/UserService.java
+++ src/com/bossky/ocean/user/UserService.java
@@ -2,31 +2,41 @@ package com.bossky.ocean.user;
 
 import com.bossky.ocean.ext.ResultPage;
 
-/**用户业务接口
+/**
+ * 用户业务接口
+ * 
  * @author Zhao_Gq
  */
 public interface UserService {
-	/**添加用户
-	 * @param username 用户账号
-	 * @param password 密码
-	 * @param role 角色
-	 * @return
-	 * 	User对象
+	/**
+	 * 添加用户
+	 * 
+	 * @param username
+	 *            用户账号
+	 * @param password
+	 *            密码
+	 * @param role
+	 *            角色
+	 * @return User对象
 	 */
-	public User addUser(String username,String password,Integer role);
-	
-	
-	/**查询用户
-	 * @param username 用户名（邮箱）
+	public User addUser(String username, String password, Integer role);
+
+	/**
+	 * 查询用户
+	 * 
+	 * @param username
+	 *            用户名（邮箱）
 	 * @return
 	 */
 	public ResultPage<User> listUser(String username);
-	
-	/**获取单个用户对象
-	 * @param id 用户ID
-	 * @return
-	 * 	User对象
+
+	/**
+	 * 获取单个用户对象
+	 * 
+	 * @param id
+	 *            用户ID
+	 * @return User对象
 	 */
 	public User getUser(String id);
-	
+
 }
