2855594cb3454773603d4e9d59bb51e36c750126
HelloWorld <bossky@users.noreply.github.com>
2014-12-08 21:17:42 +0800

    优化代码

--- src/com/bossky/util/TimeUtil.java
+++ src/com/bossky/util/TimeUtil.java
@@ -1,7 +1,5 @@
 package com.bossky.util;
 
-
-import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -11,7 +9,21 @@ import java.util.Date;
  *
  */
 public class TimeUtil {
+	/** 一天的毫秒数 */
 	public final static long ONE_DAY_MILLISECOND = 24 * 3600 * 1000;
+	/** 一小时的毫秒数 */
+	public final static long ONE_HOUR_MILLISECOND = 3600000;
+
+	/**
+	 * ２个日期是否为同一个
+	 * 
+	 * @param date
+	 * @param otherdate
+	 * @return
+	 */
+	public static boolean isSameDay(Date date, Date otherdate) {
+		return calDay(date, otherdate) == 0;
+	}
 
 	/**
 	 * 计算２个日期相差的天数 如果 date比otherdate返回正数 date比otherdate大返回负数
@@ -25,22 +37,4 @@ public class TimeUtil {
 		return (int) (offset / ONE_DAY_MILLISECOND);
 	}
 
-	/**
-	 * ２个日期是否为同一个
-	 * 
-	 * @param date
-	 * @param otherdate
-	 * @return
-	 */
-	public static boolean isSameDay(Date date, Date otherdate) {
-		return calDay(date, otherdate) == 0;
-	}
-
-	public static void main(String[] args) {
-		Calendar cal = Calendar.getInstance();
-		Date d1 = cal.getTime();
-		cal.add(Calendar.DATE, 11);
-		Date d2 = cal.getTime();
-		System.out.println(calDay(d1, d2));
-	}
 }
